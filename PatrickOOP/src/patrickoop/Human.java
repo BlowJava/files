316cb047ac3520d0584bf5fc1bf648704f922e36
trickrenzgarcia <42066235+trickrenzgarcia@users.noreply.github.com>
2018-08-17 18:13:17 +0800

    Add files via upload

---
+++ PatrickOOP/src/patrickoop/Human.java
@@ -0,0 +1,20 @@
+//Java Programmed in NetBeans, with jdk1.8.0_151
+package patrickoop;
+public class Human {
+    
+    public static void main(String[] args) {
+    
+        PatrickOOP patrick;
+        
+        patrick = new PatrickOOP();
+        patrick.name = "Patrick Renz";
+        patrick.age = 17;
+        patrick.height = 56;
+        patrick.eyeColor = "Black";
+        patrick.School = "STI College Caloocan";
+        patrick.Strand = "ICT-MAWD";
+        patrick.Prog = "Java, HTML, CSS";
+        
+        patrick.speak();
+    }
+}
---
+++ PatrickOOP/src/patrickoop/PatrickOOP.java
@@ -0,0 +1,24 @@
+//Java Programmed in NetBeans, with jdk1.8.0_151
+package patrickoop;
+public class PatrickOOP {
+
+    String name; int age; int height; String eyeColor; String Strand;
+    String School; String Prog;
+    public PatrickOOP() {
+        // using this identifier to orient Main class
+    }
+    
+    public void speak(){
+        System.out.println("Hello my name "+ name);
+        System.out.println("My Strand is "+ Strand + " in "+ School);
+        System.out.println("I am "+ height + " inches tall");
+        System.out.println("I am "+ age + " years old");
+        System.out.println("My eye color is "+ eyeColor);
+        System.out.println("I Learn: "+ Prog);
+    }
+}
+
+
+
+
+
